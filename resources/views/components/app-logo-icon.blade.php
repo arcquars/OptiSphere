472ae35d9afefed4c144c12111afdce25acd1fb5
Pedro <arc.quars@gmail.com>
2025-10-28 22:33:20 -0400

    Implementar traduccion a espa;ol

--- resources/views/components/app-logo-icon.blade.php
+++ resources/views/components/app-logo-icon.blade.php
@@ -1,3 +1,3 @@
-<div class="sm:w-full md:w-1/2 mx-auto">
+<div class="sm:w-full mx-auto">
 <img src="{{ asset('img/android-chrome-192x192.png') }}" alt="Logo cericier" class="max-w-full h-auto">
 </div>
